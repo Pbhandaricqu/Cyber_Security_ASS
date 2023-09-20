362a45db2a610b168dc0d3df573410796b56df26
Prabin Bhandari <105980902+Pbhandaricqu@users.noreply.github.com>
2023-09-20 11:41:14 +1000

    Delete Peer Programming directory

--- Peer Programming/setupWDAC.ps1
+++
@@ -1,7 +0,0 @@
-﻿function enableWDAC {
-    $cipPath = "C:\Users\vagrant\Documents\CIpolicy.xml"
-    Start-Process -FilePath "citool.exe" -ArgumentList "-policy $cipPath" -Wait -NoNewWindow
-}
-enableWDAC
-testWDAC
-resetWDAC
