a5b23615998b5ab58c54d409929c1173de25bb6e
AawanAdhikari <128004419+AawanAdhikari@users.noreply.github.com>
2023-09-22 01:16:55 +1000

    Add files via upload

---
+++ ASS ess Ment/Defend/Block HTTP code.ps1
@@ -0,0 +1,11 @@
+﻿# Function to block outbound connections to TCP port 80 using Windows Firewall
+function BlockOutboundPort80 {
+    $ruleName = "BlockOutboundPort80"
+
+    New-NetFirewallRule -Name $ruleName -Action Block -Direction Outbound -Protocol TCP -LocalPort 80
+
+    Write-Host "Outbound connections to TCP port 80 are blocked."
+}
+
+# Example usage:
+BlockOutboundPort80
